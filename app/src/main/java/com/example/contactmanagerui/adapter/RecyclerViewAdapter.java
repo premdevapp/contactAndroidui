8f37ef855c5c475d7eb5568e4bf0de1530a7bd6b
premnath <premdevapp@gmail.com>
2020-11-28 09:26:55 +0530

    finished

--- app/src/main/java/com/example/contactmanagerui/adapter/RecyclerViewAdapter.java
+++ app/src/main/java/com/example/contactmanagerui/adapter/RecyclerViewAdapter.java
@@ -1,14 +1,18 @@
 package com.example.contactmanagerui.adapter;
 
 import android.content.Context;
+import android.content.Intent;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.example.contactmanagerui.DetailsActivity;
 import com.example.contactmanagerui.R;
 import com.example.contactmanagerui.model.Contact;
 
@@ -43,13 +47,41 @@ public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapte
         return contactList.size();
     }
 
-    public class ViewHolder extends RecyclerView.ViewHolder{
+    public class ViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
         public TextView name_contact;
         public TextView phoneNumber;
+        public ImageView iconButton;
         public ViewHolder(@NonNull View itemView) {
             super(itemView);
+           itemView.setOnClickListener(this);
             name_contact = itemView.findViewById(R.id.name);
             phoneNumber = itemView.findViewById(R.id.phone);
+            iconButton = itemView.findViewById(R.id.icon_button);
+            iconButton.setOnClickListener(this);
+        }
+
+        @Override
+        public void onClick(View v) {
+            int pos = getAdapterPosition();
+            Contact contact = contactList.get(pos);
+
+            Intent intent = new Intent(context, DetailsActivity.class);
+            intent.putExtra("name", contact.getName());
+            intent.putExtra("phone", contact.getPhoneNumber());
+
+            context.startActivity(intent);
+
+            /*switch (v.getId()){
+
+                case R.id.cardView:
+                    Log.d("click", "Whole onClick: " + contact.getName() + " " + contact.getPhoneNumber());
+                    break;
+                case R.id.icon_button:
+                    Log.d("click", "ICON onClick: " + contact.getName() + " " + contact.getPhoneNumber());
+                    break;
+
+            }*/
+
         }
     }
 }
